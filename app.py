a3fb518ad431e759d0245c9d6a0e1160b428f360
Pranav <pranav.pakanati@gmail.com>
2021-11-21 21:13:50 -0600

    Hot reload server on file change

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+from os import path, walk
+
 from flask import Flask, render_template
 from flask_restful import Api
 
 
 app = Flask(__name__, static_url_path='/static', static_folder='app/static/', template_folder='app/templates/')
+watch_dirs = ['app/static', 'app/templates']
+watch_files = []
+ignore_watch = ['stylesheet.css.map']
+for dir in watch_dirs:
+    for location, _, files in walk(dir):
+        for file in files:
+            if file not in ignore_watch:
+                filename = path.join(location, file)
+                watch_files.append(filename)
 Api(app)
 
 
@@ -12,4 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True, extra_files=watch_files)
